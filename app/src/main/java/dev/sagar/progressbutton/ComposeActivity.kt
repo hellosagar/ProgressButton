4aae050b877be8f30d5ab51f009740e90ed94c08
Vaibhav2002 <vaibhavsam2511@gmail.com>
2021-07-06 18:33:51 +0530

    wildcard import fixing

--- app/src/main/java/dev/sagar/progressbutton/ComposeActivity.kt
+++ app/src/main/java/dev/sagar/progressbutton/ComposeActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.* // ktlint-disable no-wildcard-imports
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.* // ktlint-disable no-wildcard-imports
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
